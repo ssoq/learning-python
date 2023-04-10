b2a3ff779d56797a7caadd89f1f0c188b8737bff
sso <93847254+ssoq@users.noreply.github.com>
2023-04-10 23:56:47 +0100

    Create SwitchStatements.py

---
+++ SwitchStatements.py
@@ -0,0 +1,14 @@
+number = 20
+
+def NumberSwitcher():
+    match number:
+        case 10:
+            print("Youve got " + str(number))
+        case 20:
+            print("Now you have " + str(number))
+        case 30:
+            print("You have now " + str(number))
+        case 40:
+            print("You have " + str(number))
+
+NumberSwitcher()

d9d064d768ce05242b3e13fcdf9d14e9c4d2ecc7
sso <93847254+ssoq@users.noreply.github.com>
2023-04-11 12:58:44 +0100

    Create EightBall.py

---
+++ programs/EightBall.py
@@ -0,0 +1,29 @@
+# this is a basic eight ball program
+
+import random
+
+def UserQuestion():
+    print("Ask me any question")
+    question = input()
+    return question
+
+def BallChoices():
+    print("You asked me: " + UserQuestion())
+    roll = random.randint(1, 6)
+    print("You rolled a " + str(roll))
+
+    match roll:
+        case 1:
+            print("Very much likely")
+        case 2:
+            print("I'm pretty sure")
+        case 3:
+            print("I am not so sure")
+        case 4:
+            print("I don't think so")
+        case 5:
+            print("Maybe not")
+        case 6:
+            print("I have but a clue")
+
+BallChoices()

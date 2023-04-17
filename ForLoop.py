4eff080ca3d00342fdff4afa63a508493063fd9c
Sk8 <93847254+ssoq@users.noreply.github.com>
2023-04-17 15:11:50 +0100

    new features learnt with forloops

--- ForLoop.py
+++ ForLoop.py
@@ -1,5 +1,7 @@
 # simple program that iterates through numbers, one at a time
 
+import random
+
 number = 0
 
 for number in range(5):
@@ -12,3 +14,13 @@ for i in range(5):
     print("Hello, how do you do?")
 
 # tadaaaaa, just like that
+
+for lewis in range(1, 10):
+    if lewis == 9:
+        print(lewis)
+    continue # only prints 9 as continue excepts all other numbers
+
+for lewis in range(10):
+    print(lewis)
+    if lewis == 5:
+        break # stops the loop after lewis == 5
